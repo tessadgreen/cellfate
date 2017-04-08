ea35ced4dba1559293097507cd08b3d8939b6947
ycyam <yyam@g.harvard.edu>
2017-04-08 08:07:28 -0400

    added test

--- cellfate/test/test.py
+++ cellfate/test/test.py
@@ -1,8 +1,8 @@
 from unittest import TestCase
 
-import cell-fate-decision
+import cellfate
 
 class Test(TestCase):
     def test_is_string(self):
-        s = cell-fate-decision.test.test_print()
+        s = cellfate.test.test_print()
         self.assertTrue(isinstance(s, basestring))
