2d676bac5e8f27a10b2c3ad79d7ba71d30bb0283
Tessa <tdgreen@tdgreens-MacBook-Pro.local>
2017-04-12 16:23:30 -0400

    beginnings of a test-data generator, io file

---
+++ cellfate/test/test-data-generator.py
@@ -0,0 +1,24 @@
+def gen_data(k, N_bins)
+	#test set of model parameter k
+	#k = [k_entry, k_division, k_departure, k_bg, k_gb, k_silence]
+	k=[0.2, 0.05, 0.14, 0.25, 0.5]
+
+
+	#starting bin occupancies
+	N_bins=10
+
+	for i=1:N_bins
+
+
+	#generate random nuclei locations and colorings 
+	#satisfying given bin fractions
+
+	#assign to bins
+
+	#increment using diffeq plus noise terms
+
+	#generate random nuclei locations in bins 
+
+
+	#return 
+
