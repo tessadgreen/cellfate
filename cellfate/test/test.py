e03550bce7bd9212811d705cca321c8c575dc766
Tessa <tdgreen@tdgreens-MacBook-Pro.local>
2017-04-17 18:59:56 -0400

    io test added

--- cellfate/test/test.py
+++ cellfate/test/test.py
@@ -1,8 +1,18 @@
 from unittest import TestCase
 
 import cellfate
+from cellfate import io
+from cellfate import model 
 
 class Test(TestCase):
     def test_is_string(self):
         s = cellfate.test_print()
         self.assertTrue(isinstance(s, str))
+
+    def test_io(self):
+    	x = io.read('test-data','R','G',5,10)
+    	assert isinstance(x.cellwidth,int)
+    	assert x.bin_num==10
+
+    def test(self):
+    	assert True
