a12c8eff388bbf1cc9ac8dc62fa63a7bbe471383
shrocklee <lsh@Seungs-MacBook-Pro.local>
2017-04-24 13:00:40 -0400

    model.py revision
    Removed all the parts that were commented out

--- cellfate/model.py
+++ cellfate/model.py
@@ -6,15 +6,6 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 from scipy.integrate import odeint
 import pandas as pd
-from cellfate import io, cell_density_object as cdo
-
-#test_params = [0, # k_ent
-#               0.05, # k_div
-#               0, # k_dep
-#               0.6, # k_bg
-#               0.1, # k_br
-#               0 # k_loss
-#               ]
 
 def pd2np(input_data):
     '''
@@ -39,10 +30,11 @@ def np2pd(input_np, binNum):
                                          in order of left to right and top to bottom)
     
     Arguments:
-        input_np: ndarray of form [red, green, both] x (binDiv x binDiv) x(time)
+        input_np: ndarray of form (3, binDiv, binDiv, time)
+                  i.e. [red, green, both] x (binDiv x binDiv) x(time)
     '''
     # Set up the column structure for dataframe
-    cols=pd.MultiIndex.from_tuples([ (x,y) for x in ['R','G','Both'] \
+    cols=pd.MultiIndex.from_tuples([ (x,y) for x in ['Sox2','Oct4','Both'] \
                                     for y in np.arange(binNum*binNum)])
     reshaped = np.reshape(input_np, (3*binNum**2, -1))
     return pd.DataFrame(reshaped.T,columns=cols)
@@ -59,17 +51,12 @@ def diffeq(w, t, p):
         p :  vector of the parameters:
                   p = [k_ent,k_div,k_dep,k_bg,k_br, k_loss]
     """
-    red, grn, both = w
-#    k_ent, k_div, k_dep, k_bg, k_br, k_loss = p
+    grn, red, both = w
     k_div, k_bg, k_br = p
 
     # Create f = (red,grn,both) - order should be same as in w vector:
-#    f = [k_ent*red + (k_div - k_dep)*red + (k_br+k_loss)*both,
-#         k_ent*grn + (k_div - k_dep)*grn + (k_bg-k_loss)*both,
-#         k_ent*both + (k_div - k_dep)*both - (k_bg+k_br-k_loss)*both]
-
-    f = [k_div*red + k_br*both,
-         k_div*grn + k_bg*both,
+    f = [k_div*grn + k_bg*both,
+         k_div*red + k_br*both,
          k_div*both - (k_bg+k_br)*both]
 
     return f
@@ -115,13 +102,10 @@ def log_prior(theta):
         theta: model parameters (specified as a list)
     """
     # unpack the model parameters
-#    k_ent, k_div, k_dep, k_bg, k_br, k_loss = theta
     k_div, k_bg, k_br = theta
   
     # We can ignore normalization factor since it is constant.
     # So we simply return 0 for parameters in the specified range.
-#    if 0 <= k_ent <= 1 and 0 <= k_div <= 1 and 0 <= k_dep <= 1  \
-#    and 0 <= k_bg <= 1 and 0 <= k_br <= 1 and 0 <= k_loss <= 1:
     if 0 <= k_div <= 1 and 0 <= k_bg <= 1 and 0 <= k_br <= 1:
         return 0.0
     return -np.inf
@@ -158,13 +142,18 @@ def log_posterior(theta, data, sigma_n):
         return -np.inf
     
     return lp + log_likelihood(theta, data, sigma_n)
+
+def negative_log_posterior(theta, data, sigma_n):
+    return -log_posterior(theta, data, sigma_n)
+
     
 def plotMap(grid, duration, plotNum=10):
     '''
     Plot the heatmap of each type of cell over time
     
     Arguments:
-        grid: diffeq solution arrays ([red,grn,both] x (binNum x binNum) x time)
+        grid: diffeq solution arrays (3, binDiv, binDiv, time)
+                            i.e. ([red,grn,both] x (binNum x binNum) x time)
         duration :  total timesteps
         plotNum : number of snapshots (plots). 
                   The time difference between each snapshot is even.    
@@ -176,8 +165,6 @@ def plotMap(grid, duration, plotNum=10):
     both = grid[2,:,:]
     # Plot heatmap for each time i*time_step
     for i in range(plotNum):
-#        print(i*time_step)
-#        print(np.shape(both))
         plt.subplot(plotNum,3,1+i*3)
         sns.heatmap(both[:,:,i*time_step], vmin=0, vmax=30,
                     annot=True, fmt='.1f', cmap="Oranges")
@@ -189,70 +176,3 @@ def plotMap(grid, duration, plotNum=10):
         plt.subplot(plotNum,3,3+i*3)
         sns.heatmap(grn[:,:,i*time_step], vmin=0, vmax=30,
                     annot=True, fmt='.1f', cmap="Greens")
-
-#######################################
-# Following codes are for test purpose#
-# You can ignore them                 #
-#######################################
-
-#testdat = io.read('test-data.mat', 'R', 'G', 5, 2)
-#testdat_grid = np.reshape(testdat.data.as_matrix(), (3,testdat.bin_num,testdat.bin_num))
-
-#testdat_solved = diffeqSolve(test_params, testdat)
-
-#params = [0, 0.1, 0, 0.6, 0.2, 0]
-#
-#
-#print(np.shape(testdat_solved))
-#cols=pd.MultiIndex.from_tuples([ (x,y) for x in ['R','G','Both'] for y in np.arange(2*2)])
-#reshaped = np.reshape(testdat_solved, (12, -1))
-#df = pd.DataFrame(reshaped.T,columns=cols)
-#df.to_pickle('sample.pkl')
-#print(np.size(testdat_grid))
-
-#testobject = cdo.CellDen(pd.read_pickle('sample.pkl'), 5)
-#lpos = log_posterior(test_params, testobject, 1)
-#print(lpos)
-
-#tt = diffeqSolve(test_params, testobject)
-#print(np2pd(tt))
-
-#print(df)
-
-#test_params = [0.1, # k_div
-#               0.4, # k_bg
-#               0.2, # k_br
-#               ]
-#testdat.tot_time=30
-#testdat_solved = diffeqSolve(test_params, testdat)
-#df = np2pd(testdat_solved, 2)
-#df.to_pickle('sample_2x2_30.pkl')
-#plotMap(testdat_solved, testdat.tot_time, 3)
-
-#testobject = cdo.CellDen(pd.read_pickle('sample_2x2_30.pkl'), 5)
-#import scipy.integrate as integrate
-#def likelihood(z, y, x, data, sigma_n):
-#    return np.exp(log_likelihood([x,y,z], data, sigma_n))
-#
-#a = integrate.tplquad(likelihood, 0, 0.2,
-#                      lambda x: 0.3, lambda x: 0.5,
-#                      lambda x,y: 0.1, lambda x,y: 0.3,
-#                      args=(testobject, 0.4))[0]
-#print(a)
-#
-
-#plt.figure()
-## Plot the number of each cell over time in each bin        
-#for i in range(binNum):
-#    for j in range(binNum):
-#        plt.subplot(4, 4, 1+i*4+j)
-#        plt.xlabel('t')
-#        plt.ylabel('n')
-#        lw = 1
-#        b = final_grid[i,j,0,:]
-#        r = final_grid[i,j,1,:]
-#        g = final_grid[i,j,2,:]
-#        
-#        plt.plot(t, b, 'orange', linewidth=lw)
-#        plt.plot(t, g, 'g', linewidth=lw)
-#        plt.plot(t, r, 'r', linewidth=lw)
