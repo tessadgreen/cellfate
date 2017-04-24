7b9e5b7af2cfd65472f926d450e00ea78c4cb64d
shrocklee <lsh@Seungs-MacBook-Pro.local>
2017-04-24 14:30:57 -0400

    Updates
    1. cell_density_fun.py
    I reduced the size of markers of scatter plot.
    
    2. cell_density_object.py
    I added two functions in the class:
       1) pd2np: changes pandas data frame into numpy array and returns it
       2) plotMap: draw heat maps of each cell at each bin.
    
    3. test.py
    I cleared out all the commented out lines.

--- cellfate/test/test.py
+++ cellfate/test/test.py
@@ -17,48 +17,13 @@ class Test(TestCase):
         nbins=6
         testdat=io.read('io-test.csv','Sox2','Oct4',nbins)
         test_params=[0.05, 0.6, 0.1]
-        #test_params = [0, # k_ent
-        #       0.05, # k_div
-        #       0, # k_dep
-        #       0.6, # k_bg
-        #       0.1, # k_br
-        #       0 # k_loss
-        #       ]
         testdat_solved = model.diffeqSolve(test_params, testdat)
         assert np.shape(testdat_solved)==(3,nbins,nbins,3)
     
     def test_model(self):
-        #test_params = [0, # k_ent
-        #    0.03, # k_div
-        #    0, # k_dep
-        #    0.1, # k_bg
-        #    0.7, # k_br
-        #    0 # k_loss
-        #       ]
         test_params=[0.03,0.1,0.7]
         nbins=4
         testdat=io.read('io-test.csv','Sox2','Oct4',nbins)
         sigma_n=0.2
         val=model.log_likelihood(test_params, testdat, sigma_n)
         assert isinstance(val,float)
-
-#testdat = io.read('test-data.mat', 'R', 'G', 5, 2)
-#testdat_grid = np.reshape(testdat.data.as_matrix(), (3,testdat.bin_num,testdat.bin_num))
-#testdat_solved = diffeqSolve(test_params, testdat_grid)
-#params = [0, 0.1, 0, 0.6, 0.2, 0]
-#
-#
-#cols=pd.MultiIndex.from_tuples([ (x,y) for x in ['R','G','Both'] for y in np.arange(2*2)])
-#reshaped = np.reshape(testdat_solved, (12, -1))
-#df = pd.DataFrame(reshaped.T,columns=cols)
-#df.to_pickle('sample.pkl')
-#print(np.size(testdat_grid))
-
-#testobject = cdo.CellDen(pd.read_pickle('sample.pkl'), 5)
-#lpos = log_posterior(test_params, testobject, 1)
-#print(lpos)
-
-#tt = diffeqSolve(test_params, testobject)
-#print(np2pd(tt))
-
-#print(df)
