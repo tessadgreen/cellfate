1a1c71c86e4bca0310949a29a05b33f1ff41f1e4
bigbengarber <bigbengarber@gmail.com>
2017-04-26 22:39:10 -0400

    Moved more of the io functions into io.py
    There were a bunch of functions in io calling functions in
    celldensity calling functions in io. I just moved load_data to
    io.py.

--- cellfate/io.py
+++ cellfate/io.py
@@ -2,6 +2,7 @@ import os
 import scipy.io as sio
 from cellfate import celldensity
 import numpy as np
+import pandas as pd
 
 def get_data_file_path(filename, data_dir='test'):
 
@@ -10,7 +11,27 @@ def get_data_file_path(filename, data_dir='test'):
 
     data_dir = os.path.join(start_dir,data_dir)
     return os.path.join(start_dir,data_dir,filename)
-    
+
+#import the .csv data file for cell location as dataframe
+def load_data(data_file, CellA, CellB):
+    """
+    Import raw data from a csv
+
+    data_file: filename
+    CellA: Name of the first cell type, e.g.'Sox2'
+    CellB: Name of the second cell type, e.g. 'Oct4'
+    """
+
+    both_high='Classify_Intensity_UpperQuartileIntensity_'+CellA+'_high_Intensity_UpperQuartileIntensity_'+CellB+'_high'
+    both_low='Classify_Intensity_UpperQuartileIntensity_'+CellA+'_low_Intensity_UpperQuartileIntensity_'+CellB+'_low'
+    high_CellA='Classify_Intensity_UpperQuartileIntensity_'+CellA+'_high_Intensity_UpperQuartileIntensity_'+CellB+'_low'
+    high_CellB='Classify_Intensity_UpperQuartileIntensity_'+CellA+'_low_Intensity_UpperQuartileIntensity_'+CellB+'_high'
+
+    data_loc=pd.read_csv(get_data_file_path(data_file), usecols=['ImageNumber',\
+        both_high,both_low,high_CellA,high_CellB,'Location_Center_Y','Location_Center_X'])
+
+    return (data_loc,both_high,both_low,high_CellA,high_CellB)
+
 def read(data_file, CellA, CellB, BinDiv, ImgWidth=1024):
     '''
     Parameters:
@@ -28,27 +49,25 @@ def read(data_file, CellA, CellB, BinDiv, ImgWidth=1024):
         Locations:
             'Location_Center_X'
             'Location_Center_Y'
-            
+
     CellA: Name of the first cell type, e.g.'Sox2'
     CellB: Name of the second cell type, e.g. 'Oct4'
-    BinDiv: An integer telling the function to divide the orginal cell image into BinDiv x BinDiv bins     
+    BinDiv: An integer telling the function to divide the orginal cell image into BinDiv x BinDiv bins
     ImgWidth: the width dimension of the image in pixels (e.g. for an image of 1024x1024, just enter 1024)
-   
+
     Returns:
     -----------
     An object with the following attributes:
-        
-    CellDen.data: A dataframe with the density of different types of cells in the bins of the 
+
+    CellDen.data: A dataframe with the density of different types of cells in the bins of the
     original cell image at different time t.The format of the output dataframe would be like --
         Rows-> time index
         Main Column-> cell types
         Sub Column-> bin index
-    
+
     CellDen.cellname: a tuple of the first and second cell name
     CellDen.bin_num: total bin number
     CellDen.tot_time: total time duratiron of the experiment
     '''
     data=celldensity.cell_density(data_file,CellA,CellB,BinDiv,ImgWidth)
     return celldensity.CellDen(data)
-
-
